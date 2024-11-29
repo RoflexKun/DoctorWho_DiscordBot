daabfee9b4d75bb3d25469dccb8932934108dddd
RoflexKun <razvan.vilcu2004@gmail.com>
2024-11-29 14:34:14 +0200

    Create .json for episodes and .txt for quotes, testing things

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+use std::{fs, io};
+use serde::Deserialize;
+use serde_json;
+
+#[derive(Debug, Deserialize)]
+struct Episode {
+    title: String,
+    runtime: i32, 
+    season: i32, 
+    episode: i32,
+}
+
+fn print_episode() -> Result<(), io::Error>
+{
+    let episodes_json = fs::read_to_string("episodes.json").unwrap();
+    let mut episodes: Vec<Episode> = serde_json::from_str(&episodes_json).expect("Eroare");  
+    for episode in episodes.iter()
+    {
+        println!("");
+        println!("{:?}", episode);
+    }
+    Ok(())
+}
+
+
 fn main() {
-    println!("A mers commit");
+    let mut user_command = String::new();
+    io::stdin().read_line(&mut user_command).expect("Error at reading user input");
+    match user_command.trim(){
+        "quote" => {println!("This command is quote");}
+        _ => {println!("We don't know this command");}
+    }
+    let _ = print_episode();
+
 }
